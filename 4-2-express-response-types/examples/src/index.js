12e8428c3aeb7f9d6c3bbdfd943bd090cbc97311
Corina <cborcoci@gmail.com>
2021-02-16 11:31:07 +0100

    Add response types exercise

---
+++ 4-2-express-response-types/examples/src/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+
+// create server
+const app = express();
+
+// set express middleware
+app.use(express.json());
+app.use(cors());
+
+// create app server
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+// endpoints, more info about express response: https://expressjs.com/es/api.html#res
+
+app.get('/response-a', (req, res) => {
+  res.json({ result: 'Ok' });
+});
+
+app.get('/response-b', (req, res) => {
+  const htmlCode= `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Express</title>
+      <link rel="stylesheet" href="http://beta.adalab.es/resources/stylesheets/all.css" >
+    </head>
+    <body class="page">
+        <h1>Esta es una página de prueba</h1>
+    </body>
+  </html>`;
+  res.send(htmlCode);
+});
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const randomNumber = getRandomNumber(10);
+
+app.get('/response-c', (req, res) => {
+  if (randomNumber%2 === 0) {
+    res.redirect('https://youtube.com')
+  } else {
+    res.redirect('https://instagram.com')
+  }
+});
+
+
+// Cuando la usuaria haga un GET a /response-d
+// con un query param user=1 o user=2 debe responder con un json con status 200 y respuesta { result: 'ok' }.
+// si se llama a este endpoint sin query param o con un query param diferente de user=1 o user=2 debe responder un json { result: 'error: invalid query param' } con el status 404.
+
+
+app.get('/response-d', (req, res) => {
+  // more info about http status codes: https://developer.mozilla.org/es/docs/Web/HTTP/Status
+  
+ if ((req.query.params.user === 1) || (req.query.params.user === 2)){
+   res.status(200).json({ result: "ok"})
+ } else {
+   res.status(404).json({
+  result: 'error: invalid query param'
+   })};
+});
